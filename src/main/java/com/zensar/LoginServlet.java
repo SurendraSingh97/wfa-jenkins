89f1c531e377807f601692a74be26365b69fe7f4
SurendraSingh97 <ssjhiouts.1696@gmail.com>
2021-03-11 14:24:10 +0530

    first commit

---
+++ src/main/java/com/zensar/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.zensar;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Test !!!");
+		
+		String user = request.getParameter("uname");
+		String pass = request.getParameter("password");
+		
+		if(user.equals("Zensar") && pass.equals("1234"))
+			response.getWriter().print("<h2>Welcome</h2>");
+			else
+				response.getWriter().print("<h2>Faileds</h2>");
+	}
+
+}
